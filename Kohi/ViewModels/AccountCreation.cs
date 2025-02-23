84d6a4885cd46f6750e1e6fa79fbfcb950541a55
nezuni1812 <132083873+nezuni1812@users.noreply.github.com>
2025-02-23 14:30:44 +0700

    simple Navigation and example codes

---
+++ Kohi/ViewModels/AccountCreation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kohi.Models;
+
+namespace Kohi.ViewModels
+{
+    internal class AccountCreation
+    {
+        public Account newAccount { get; set; }
+        public AccountCreation() {
+            newAccount = new Account();
+        }
+    }
+}
