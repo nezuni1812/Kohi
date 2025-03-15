a46dad2ef6ba7376f07ee54067edfc352ec8604f
nezuni1812 <132083873+nezuni1812@users.noreply.github.com>
2025-03-15 23:04:17 +0700

    init UI

---
+++ Kohi/Views/IncomeExpensePage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Kohi.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class IncomeExpensePage : Page
+    {
+        public ThuViewModel ThuVM { get; set; } = new ThuViewModel();
+        public ChiViewModel ChiVM { get; set; } = new ChiViewModel();
+
+        public IncomeExpensePage()
+        {
+            this.InitializeComponent();
+            GridContent.DataContext = ThuVM;
+        }
+
+        private void SelectorBar_SelectionChanged(SelectorBar sender, SelectorBarSelectionChangedEventArgs args)
+        {
+            if (sender.SelectedItem == IncomeSelectorBar)
+            {
+                ReceiptCategory.Text = "Loại phiếu thu";
+                Amount.Text = "Số tiền thu";
+                addButtonTextBlock.Text = "Thêm phiếu thu";
+                GridContent.DataContext = ThuVM;
+            }
+            else if (sender.SelectedItem == ExpenseSelectorBar)
+            {
+                ReceiptCategory.Text = "Loại phiếu chi";
+                Amount.Text = "Số tiền chi";
+                addButtonTextBlock.Text = "Thêm phiếu chi";
+                GridContent.DataContext = ChiVM;
+            }
+        }
+
+        private void addButton_click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+
+    public class ThuViewModel
+    {
+    }
+    public class ChiViewModel
+    {
+    }
+
+}
