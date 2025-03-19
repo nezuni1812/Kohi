7062cb7757fc6c14e2954540158b99f9cd81e6f4
nezuni1812 <132083873+nezuni1812@users.noreply.github.com>
2025-03-19 19:52:34 +0700

    update use table view for displaying data

---
+++ Kohi/ViewModels/ExpenseViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Kohi.Models;
+using Kohi.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kohi.ViewModels
+{
+    public class ExpenseViewModel
+    {
+        private IDao _dao;
+        public ObservableCollection<ExpenseModel> ExpenseReceipts { get; set; }
+
+        public ExpenseViewModel()
+        {
+            _dao = Service.GetKeyedSingleton<IDao>();
+            ExpenseReceipts = new ObservableCollection<ExpenseModel>();
+
+            LoadReceipts();
+        }
+
+        private async void LoadReceipts()
+        {
+            await Task.Delay(0);
+            var receipts = _dao.Expenses.GetAll();
+            ExpenseReceipts.Clear();
+
+            foreach (var receipt in receipts)
+            {
+                ExpenseReceipts.Add(receipt);
+            }
+        }
+    }
+}
--- Kohi/Views/IncomeExpensePage.xaml.cs
+++ Kohi/Views/IncomeExpensePage.xaml.cs
@@ -12,6 +12,10 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Kohi.ViewModels;
+using WinUI.TableView;
+using Kohi.Models;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,43 +27,47 @@ namespace Kohi.Views
     /// </summary>
     public sealed partial class IncomeExpensePage : Page
     {
-        public ThuViewModel ThuVM { get; set; } = new ThuViewModel();
-        public ChiViewModel ChiVM { get; set; } = new ChiViewModel();
-
+        public IncomeViewModel IncomeViewModel { get; set; } = new IncomeViewModel();
+        public ExpenseViewModel ExpenseViewModel { get; set; } = new ExpenseViewModel();
         public IncomeExpensePage()
         {
             this.InitializeComponent();
-            GridContent.DataContext = ThuVM;
+            //GridContent.DataContext = IncomeViewModel;
+        }
+
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is TableView tableView && tableView.SelectedItem is ExpenseModel selectedExpense)
+            {
+                int id = selectedExpense.ExpenseCategoryId; //Category nha 
+                Debug.WriteLine($"Selected Expense ID: {id}");
+            }
         }
 
         private void SelectorBar_SelectionChanged(SelectorBar sender, SelectorBarSelectionChangedEventArgs args)
         {
             if (sender.SelectedItem == IncomeSelectorBar)
             {
-                ReceiptCategory.Text = "Loại phiếu thu";
-                Amount.Text = "Số tiền thu";
+                //ReceiptCategory.Text = "Loại phiếu thu";
+                //Amount.Text = "Số tiền thu";
                 addButtonTextBlock.Text = "Thêm phiếu thu";
-                GridContent.DataContext = ThuVM;
+                
+
+                //GridContent.DataContext = IncomeViewModel;
             }
             else if (sender.SelectedItem == ExpenseSelectorBar)
             {
-                ReceiptCategory.Text = "Loại phiếu chi";
-                Amount.Text = "Số tiền chi";
+                //ReceiptCategory.Text = "Loại phiếu chi";
+                //Amount.Text = "Số tiền chi";
                 addButtonTextBlock.Text = "Thêm phiếu chi";
-                GridContent.DataContext = ChiVM;
+                MyTableView.ItemsSource = ExpenseViewModel.ExpenseReceipts; 
+                //GridContent.DataContext = ExpenseViewModel;
             }
         }
 
         private void addButton_click(object sender, RoutedEventArgs e)
         {
         }
-    }
-
-    public class ThuViewModel
-    {
-    }
-    public class ChiViewModel
-    {
     }
 
 }
