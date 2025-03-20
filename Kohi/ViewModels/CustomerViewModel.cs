0adc559fe6259198ec81e3b90931288ed8cdcfa4
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-03-20 11:42:15 +0700

    Add customer tableview and viewmodel

---
+++ Kohi/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Kohi.Models;
+using Kohi.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kohi.ViewModels
+{
+    public class CustomerViewModel
+    {
+        private IDao _dao;
+        public ObservableCollection<CustomerModel> Customers { get; set; }
+
+        public CustomerViewModel()
+        {
+            _dao = Service.GetKeyedSingleton<IDao>();
+            Customers = new ObservableCollection<CustomerModel>();
+
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            await Task.Delay(0);
+            var customers = _dao.Customers.GetAll();
+            Customers.Clear();
+
+            foreach (var customer in customers)
+            {
+                Customers.Add(customer);
+            }
+        }
+    }
+}
--- Kohi/Views/CustomersPage.xaml.cs
+++ Kohi/Views/CustomersPage.xaml.cs
@@ -12,6 +12,10 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Kohi.ViewModels;
+using WinUI.TableView;
+using Kohi.Models;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +27,22 @@ namespace Kohi.Views
     /// </summary>
     public sealed partial class CustomersPage : Page
     {
+        public CustomerViewModel CustomerViewModel { get; set; } = new CustomerViewModel();
         public CustomersPage()
         {
             this.InitializeComponent();
+            //GridContent.DataContext = IncomeViewModel;
+        }
+        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is TableView tableView && tableView.SelectedItem is ExpenseModel selectedExpense)
+            {
+                int id = selectedExpense.ExpenseCategoryId; //Category nha 
+                Debug.WriteLine($"Selected Expense ID: {id}");
+            }
+        }
+        private void addButton_click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
