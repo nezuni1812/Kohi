68f27a02bfab95f742e205139533f1f6dc546d7c
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-03-21 20:04:20 +0700

    update 4 page inventory

---
+++ Kohi/ViewModels/InventoryViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Kohi.Models;
+using Kohi.Services;
+using Kohi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kohi.ViewModels
+{
+    public class InventoryViewModel
+    {
+        private IDao _dao;
+        public FullObservableCollection<InventoryModel> Inventories { get; set; }
+
+        public InventoryViewModel()
+        {
+            _dao = Service.GetKeyedSingleton<IDao>();
+            Inventories = new FullObservableCollection<InventoryModel>();
+
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            // Giả lập tải dữ liệu không đồng bộ từ MockDao
+            await Task.Delay(1); // Giả lập delay để giữ async
+            var inventories = _dao.Inventories.GetAll();
+            Inventories.Clear();
+
+            foreach (var inventory in inventories)
+            {
+                Inventories.Add(inventory);
+            }
+        }
+    }
+}
