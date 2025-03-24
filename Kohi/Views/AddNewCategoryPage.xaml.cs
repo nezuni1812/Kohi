e04a0082560d53ecb5be329510580eca86b54054
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-03-24 21:58:33 +0700

    Update show image

--- Kohi/Views/AddNewCategoryPage.xaml.cs
+++ Kohi/Views/AddNewCategoryPage.xaml.cs
@@ -1,4 +1,4 @@
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -14,6 +14,8 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage.Pickers;
 using Windows.Storage;
+using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.Storage.Streams;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -30,18 +32,41 @@ namespace Kohi.Views
             this.InitializeComponent();
         }
 
-        private async void AddImageButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        private async void AddImageButton_Click(object sender, RoutedEventArgs e)
         {
+            // Initialize the picker
             var picker = new FileOpenPicker();
-            Image img = sender as Image;
-
             picker.ViewMode = PickerViewMode.Thumbnail;
-
             picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
 
-            //picker.FileTypeFilter.Add(".jpg");
+            // WinUI 3 requires a window handle to initialize file pickers
+            // Get the current window handle
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(App.MainWindow);
+            // Initialize the picker with the window handle
+            WinRT.Interop.InitializeWithWindow.Initialize(picker, hwnd);
 
+            // Pick a file
             StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Load the image into an image control
+                using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    await bitmapImage.SetSourceAsync(stream);
+                    mypic.Source = bitmapImage;
+
+                    // You can store the file path or file itself for later use if needed
+                    // For example, if you want to save the file path to your product model:
+                    // Product.ImagePath = file.Path;
+
+                    // You can also update the UI to show the selected file name
+                    outtext.Text = "Ảnh đã chọn: " + file.Name;
+                }
+            }
         }
 
         private void saveButton_click(object sender, RoutedEventArgs e)
