7f75b261f2edaec4c5b9db3507bb9cb3c3007d50
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-03-26 16:36:56 +0700

    Update ProductVariant and RecipeDetail ViewModel

---
+++ Kohi/ViewModels/RecipeDetailViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Kohi.Models;
+using Kohi.Services;
+using Kohi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kohi.ViewModels
+{
+    public class RecipeDetailViewModel
+    {
+        private IDao _dao;
+        public FullObservableCollection<RecipeDetailModel> RecipeDetails { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public int TotalItems { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems / PageSize); // Tổng số trang
+        public RecipeDetailViewModel()
+        {
+            _dao = Service.GetKeyedSingleton<IDao>();
+            RecipeDetails = new FullObservableCollection<RecipeDetailModel>();
+
+            LoadData();
+        }
+
+        public async Task LoadData(int page = 1)
+        {
+            CurrentPage = page;
+            TotalItems = _dao.RecipeDetails.GetCount(); // Lấy tổng số khách hàng từ DAO
+            var result = await Task.Run(() => _dao.RecipeDetails.GetAll(
+                pageNumber: CurrentPage,
+                pageSize: PageSize
+            )); // Lấy danh sách khách hàng phân trang
+            RecipeDetails.Clear();
+            foreach (var item in result)
+            {
+                RecipeDetails.Add(item);
+            }
+        }
+
+        // Phương thức để chuyển đến trang tiếp theo
+        public async Task NextPage()
+        {
+            if (CurrentPage < TotalPages)
+            {
+                await LoadData(CurrentPage + 1);
+            }
+        }
+
+        // Phương thức để quay lại trang trước
+        public async Task PreviousPage()
+        {
+            if (CurrentPage > 1)
+            {
+                await LoadData(CurrentPage - 1);
+            }
+        }
+
+        // Phương thức để chuyển đến trang cụ thể
+        public async Task GoToPage(int page)
+        {
+            if (page >= 1 && page <= TotalPages)
+            {
+                await LoadData(page);
+            }
+        }
+    }
+}
