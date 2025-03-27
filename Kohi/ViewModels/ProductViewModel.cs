e5da8623273faf045e49f40ac2f0a260e447867e
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-03-27 21:54:36 +0700

    Add product function

--- Kohi/ViewModels/ProductViewModel.cs
+++ Kohi/ViewModels/ProductViewModel.cs
@@ -142,6 +142,7 @@ namespace Kohi.ViewModels
             try
             {
                 int result = _dao.Products.Insert($"{TotalItems + 1}", product);
+                product.Category = _dao.Categories.GetAll().FirstOrDefault(c => c.Id == product.CategoryId);
             }
             catch (Exception ex)
             {
