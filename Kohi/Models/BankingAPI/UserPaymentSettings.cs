d38aa7fc1700b28ca72c7d2c19b55b03e0ecaa7f
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-04-11 11:33:09 +0700

    Update local storage for bank info and adress

---
+++ Kohi/Models/BankingAPI/UserPaymentSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kohi.Models.BankingAPI
+{
+    public class UserPaymentSettings
+    {
+        public string AccountNo { get; set; }
+        public string AccountName { get; set; }
+        public string BankBin { get; set; } // Dùng bin để tìm lại bank
+        public string Template { get; set; }
+        public string Address { get; set; } // ✅ Thêm dòng này
+
+    }
+
+}
